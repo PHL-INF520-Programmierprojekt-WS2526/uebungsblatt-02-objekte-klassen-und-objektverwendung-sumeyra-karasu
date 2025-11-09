8b9806734181cd1891aec9bb4cacaf62321348f7
Sümeyra Karasu <8kkarasus@gmail.com>
2025-11-09 15:14:19 +0100

    petadoption ende

--- src/main/java/de/phl/programmingproject/petadoption/Main.java
+++ src/main/java/de/phl/programmingproject/petadoption/Main.java
@@ -5,11 +5,6 @@ import java.util.Date;
 /**
  * PUT YOUR CODE FOR THE EXERCISE 'Pet Adoption Center' HERE.
  */
-public class Main {
-    public static void main(String[] args) {
-        package de.phl.programmingproject.petadoption;
-
-import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
