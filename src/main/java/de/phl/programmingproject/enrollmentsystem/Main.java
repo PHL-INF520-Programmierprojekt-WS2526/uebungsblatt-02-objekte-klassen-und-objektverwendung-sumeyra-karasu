8ead09c7e765d78caeb0904e86792a3ac1345fd0
Sümeyra Karasu <8kkarasus@gmail.com>
2025-11-09 15:06:37 +0100

    enrollmentsystem ende

--- src/main/java/de/phl/programmingproject/enrollmentsystem/Main.java
+++ src/main/java/de/phl/programmingproject/enrollmentsystem/Main.java
@@ -1,16 +1,16 @@
 package de.phl.programmingproject.enrollmentsystem;
 
+import java.util.List;
+
 /**
  * PUT YOUR CODE FOR THE EXERCISE 'STUDENT ENROLLMENT SYSTEM' IN THIS CLASS
  */
-package de.phl.programmingproject.enrollmentsystem;
 
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        // 1) Student anlegen
+    // 1) Student anlegen
         Student john = new Student("John Doe", "12345");
 
         // 2) Kurs anlegen
@@ -23,50 +23,45 @@ public class Main {
         // 4) In Kurs einschreiben und Enrollment speichern
         Enrollment enrollment1 = john.enroll(introCS);
 
-        // 5) Erneut Infos ausgeben (zeigt erfolgreiche Einschreibung)
-        System.out.println("Student-Info (nach Einschreibung): " + john.getInfo());
-        System.out.println("Kurs-Info (nach Einschreibung): " + introCS.getInfo());
+        // 5) Erneut Infos ausgeben
+        System.out.println("Student nach Einschreibung: " + john.getInfo());
+        System.out.println("Kurs nach Einschreibung: " + introCS.getInfo());
 
         // 6) Enrollment-Infos ausgeben
-        System.out.println("Enrollment-Info: " + enrollment1.getInfo());
+        System.out.println(enrollment1.getInfo());
 
         // 7) Note setzen
         enrollment1.setGrade(4.0);
 
-        // 8) Enrollment-Infos erneut ausgeben (mit aktualisierter Note)
-        System.out.println("Enrollment-Info (aktualisiert): " + enrollment1.getInfo());
+        // 8) Enrollment-Infos erneut ausgeben
+        System.out.println(enrollment1.getInfo());
 
         // 9) Weiteren Kurs anlegen
         Course oop = new Course("Objektorientierte Programmierung");
 
         // 10) In neuen Kurs einschreiben
-        Enrollment enrollment2 = john.enroll(oop);
+        john.enroll(oop);
 
         // 11) Liste der Kurse des Studenten ausgeben
-        System.out.println("\nKurse von " + john.getInfo() + ":");
-        List<Course> johnsCourses = john.getCourses();
-        for (Course c : johnsCourses) {
+        System.out.println("\nKurse von " + john.getName() + ":");
+        List<Course> courses = john.getCourses();
+        for (Course c : courses) {
             System.out.println(" - " + c.getInfo());
         }
 
         // 12) Studierende im ersten Kurs ausgeben
-        System.out.println("\nStudierende in \"" + introCS.getInfo() + "\":");
-        List<Student> studentsInIntro = introCS.getStudents();
-        for (Student s : studentsInIntro) {
+        System.out.println("\nStudierende in \"" + introCS.getName() + "\":");
+        for (Student s : introCS.getStudents()) {
             System.out.println(" - " + s.getInfo());
         }
 
-        // 13) Studenten aus erstem Kurs streichen
+        // 13) Student aus erstem Kurs streichen
         introCS.drop(john); // alternativ: john.drop(introCS);
 
-        // 14) Erneut Studierende im ersten Kurs ausgeben (nach Streichen)
-        System.out.println("\nStudierende in \"" + introCS.getInfo() + "\" (nach Drop):");
-        studentsInIntro = introCS.getStudents();
-        for (Student s : studentsInIntro) {
+        // 14) Studierende erneut ausgeben (nach Drop)
+        System.out.println("\nNach dem Drop in \"" + introCS.getName() + "\":");
+        for (Student s : introCS.getStudents()) {
             System.out.println(" - " + s.getInfo());
         }
     }
-}
-
-
 }
